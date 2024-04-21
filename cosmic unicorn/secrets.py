22356e046047258545b9dc854dd0e0361bb2b224
Nota Name <email@thisisnotanemail.com>
2024-04-21 16:54:26 -0700

    added example secrets.py

---
+++ cosmic unicorn/secrets.py
@@ -0,0 +1,6 @@
+WIFI_SSID = "wifiSSID"
+WIFI_PASSWORD = "WiFi Password Here"
+WUNDERGROUNDAPIKEY = "09099abc4567efghhhh334888960abc"
+WUNDERGROUNDSTATION = "KCAMEHH420"
+TIMEZONEOFFSET = "http://worldtimeapi.org/api/ip"
+#Using ip to assume timezone. Or use something like http://worldtimeapi.org/api/timezone/America/Los_Angeles
